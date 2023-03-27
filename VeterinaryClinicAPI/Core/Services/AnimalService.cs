3530fed30aa05753e81de726f838547b1a585354
raluca.mi <raluca.ioana169@gmail.com>
2023-03-27 21:14:22 +0300

    Created animal service in Core

---
+++ VeterinaryClinicAPI/Core/Services/AnimalService.cs
@@ -0,0 +1,27 @@
+﻿using DataLayer.Repositories;
+using DataLayer.Entities;
+
+namespace Core.Services
+{
+    public class AnimalService
+    {
+        private AnimalsRepository animalsRepository { get; set; }
+        public AnimalService(AnimalsRepository animalsRepository)
+        {
+            this.animalsRepository = animalsRepository;
+        }
+
+        public List<Animal> GetAll()
+        {
+            var result = animalsRepository.GetAll();
+            return result;
+        }
+        public Animal GetById(int animalId)
+        {
+            var result = animalsRepository.GetById(animalId);
+            return result;
+        }
+
+        
+    }
+}

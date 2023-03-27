35ac82f20aab8b5edea3698e3e3b9e84a67b0d64
raluca.mi <raluca.ioana169@gmail.com>
2023-03-27 22:11:11 +0300

    Added Dtos and Mapping

---
+++ VeterinaryClinicAPI/DataLayer/Dtos/AnimalDto.cs
@@ -0,0 +1,17 @@
+﻿using DataLayer.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataLayer.Dtos
+{
+    public class AnimalDto
+    {
+        public int Id { get; set; }
+        public String Name { get; set; }
+        public String Breed { get; set; }
+        public double Weight { get; set; }
+    }
+}
---
+++ VeterinaryClinicAPI/DataLayer/Mapping/AnimalsMappingExtensions.cs
@@ -0,0 +1,46 @@
+﻿using DataLayer.Dtos;
+using DataLayer.Entities;
+using DataLayer.Enums;
+
+namespace DataLayer.Mapping
+{
+    public static class AnimalsMappingExtensions
+    {
+        public static AnimalDto ToAnimalDto(this Animal animal)
+        {
+            if (animal == null) return null;
+
+            var result = new AnimalDto();
+            result.Id = animal.Id;
+            result.Name = animal.Name;
+            result.Weight = animal.HealthCertificate.Weight;
+
+            switch (animal.Breed)
+            {
+                case Breed.Dog:
+                    result.Breed = "Dog";
+                    break;
+                case Breed.Cat:
+                    result.Breed = "Cat";
+                    break;
+                case Breed.Rabbit:
+                    result.Breed = "Rabbit";
+                    break;
+                case Breed.Hamster:
+                    result.Breed = "Hamster";
+                    break;
+                case Breed.Cow:
+                    result.Breed = "Cow";
+                    break;
+                case Breed.Horse:
+                    result.Breed = "Horse";
+                    break;
+                   
+                default:
+                    break;
+            }
+
+            return result;
+        }
+    }
+}

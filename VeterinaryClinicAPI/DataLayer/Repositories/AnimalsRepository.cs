62e393999626b744148f0e9d77d62cdd363f09cd
raluca.mi <raluca.ioana169@gmail.com>
2023-03-27 21:13:28 +0300

    Created  animals repository

---
+++ VeterinaryClinicAPI/DataLayer/Repositories/AnimalsRepository.cs
@@ -0,0 +1,25 @@
+﻿using DataLayer.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataLayer.Repositories
+{
+    public class AnimalsRepository
+    {
+        public List<Animal> GetAll()
+        {
+            var results = DbContext.Animals;
+            return results;
+        }
+
+        public Animal GetById(int animalId)
+        {
+            var result = DbContext.Animals.Where(a => a.Id == animalId).FirstOrDefault();
+            return result;
+        }
+
+    }
+}

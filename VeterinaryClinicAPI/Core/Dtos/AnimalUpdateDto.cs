a450354162f6b47e98388473bddb1d5d128295b0
raluca.mi <raluca.ioana169@gmail.com>
2023-03-27 21:31:00 +0300

    Create Dtos for Update

---
+++ VeterinaryClinicAPI/Core/Dtos/AnimalUpdateDto.cs
@@ -0,0 +1,16 @@
+﻿using DataLayer.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Dtos
+{
+    public class AnimalUpdateDto
+    {
+        public int Id { get; set; }
+        public HealthCertificateUpdateDto HealthCertificate { get; set; }
+        
+    }
+}

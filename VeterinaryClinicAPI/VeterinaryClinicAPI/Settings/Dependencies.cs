54cf6faa151225e5c5e620a66554feba107659ec
raluca.mi <raluca.ioana169@gmail.com>
2023-03-27 21:15:15 +0300

    Configured Services and Repositories for DI

---
+++ VeterinaryClinicAPI/VeterinaryClinicAPI/Settings/Dependencies.cs
@@ -0,0 +1,28 @@
+﻿using Core.Services;
+using DataLayer.Repositories;
+
+namespace VeterinaryClinicAPI.Settings
+{
+    public static class Dependencies
+    {
+        public static void Inject(WebApplicationBuilder applicationBuilder)
+        {
+            applicationBuilder.Services.AddControllers();
+            applicationBuilder.Services.AddSwaggerGen();
+
+            AddRepositories(applicationBuilder.Services);
+            AddServices(applicationBuilder.Services);
+        }
+
+        private static void AddServices(IServiceCollection services)
+        {
+            services.AddScoped<AnimalService>();
+        }
+
+        private static void AddRepositories(IServiceCollection services)
+        {
+
+            services.AddScoped<AnimalsRepository>();
+        }
+    }
+}
